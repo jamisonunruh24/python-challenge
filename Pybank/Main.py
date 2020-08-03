ea9402d43bfeac5044b855924c34ec676f89da07
Jamison Unruh <jamisonunruh24@gmail.com>
2020-08-03 18:22:20 -0500

    added money for total profit gain/loss

--- Pybank/Main.py
+++ Pybank/Main.py
@@ -5,6 +5,7 @@ import os
 # create path to file
 resources_path = os.path.join("Resources/" + "budget_data.csv")
 months = 0
+money = 0
 #read csv
 with open(resources_path) as csv_file:
     csv_reader = csv.reader(csv_file)
@@ -13,5 +14,7 @@ with open(resources_path) as csv_file:
 
     for row in csv_reader:
         months += 1
+        money = money + int(row[1])
 print(months)
+print(money)
 
