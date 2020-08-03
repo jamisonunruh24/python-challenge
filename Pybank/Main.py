6c60425fc4ece147db5c7bb887041dca181abcce
Jamison Unruh <jamisonunruh24@gmail.com>
2020-08-03 18:07:29 -0500

    added in counter for months and total votes

--- Pybank/Main.py
+++ Pybank/Main.py
@@ -4,12 +4,14 @@ import os
 
 # create path to file
 resources_path = os.path.join("Resources/" + "budget_data.csv")
-
+months = 0
 #read csv
 with open(resources_path) as csv_file:
     csv_reader = csv.reader(csv_file)
 
-    for row in csv_reader:
-        print(row)
+    csv_header = next(csv_file)
 
+    for row in csv_reader:
+        months += 1
+print(months)
 
--- Pypoll/Main.py
+++ Pypoll/Main.py
@@ -4,10 +4,13 @@ import os
 
 # create path to file
 resources_path = os.path.join("Resources/" + "election_data.csv")
-
+total = 0
 #read csv
 with open(resources_path) as csv_file:
-    csv_reader = csv.reader(csv_file)
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
+    csv_header = next(csv_file) 
 
     for row in csv_reader:
-        print(row)
+        total += 1
+print(total)
