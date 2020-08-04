9a78f3a78a3b76df45bb0b8a88219df4b03a16fa
Jamison Unruh <jamisonunruh24@gmail.com>
2020-08-03 21:37:25 -0500

    added vote counter for canidate, updated sources and problem solving

--- Pypoll/Main.py
+++ Pypoll/Main.py
@@ -5,7 +5,9 @@ import os
 # create path to file
 resources_path = os.path.join("Resources/" + "election_data.csv")
 total = 0
-canidate = []
+votes = 0
+canidates = []
+totals = []
 #read csv
 with open(resources_path) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -14,7 +16,15 @@ with open(resources_path) as csv_file:
 
     for row in csv_reader:
         total += 1
-        if row[2] not in canidate:
-            canidate.append(row[2])
+        if row[2] not in canidates:
+            canidates.append(row[2])
+        
+        for canidate in canidates:
+            if row[2] == "Khan":
+                votes += 1
+            #totals.append(votes)
+        
+    
 print(total)
-print(canidate)
+print(canidates)
+print(votes)
