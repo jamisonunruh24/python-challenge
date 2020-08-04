36abdec0bc1c6e06f2e3de8bdf470fd92b147540
Jamison Unruh <jamisonunruh24@gmail.com>
2020-08-03 20:20:39 -0500

    added greatest increase/decrease, updating problem solving list

--- Pybank/Main.py
+++ Pybank/Main.py
@@ -6,6 +6,8 @@ import os
 resources_path = os.path.join("Resources/" + "budget_data.csv")
 months = 0
 money = 0
+increase = 0
+decrease = 0
 #read csv
 with open(resources_path) as csv_file:
     csv_reader = csv.reader(csv_file)
@@ -15,6 +17,17 @@ with open(resources_path) as csv_file:
     for row in csv_reader:
         months += 1
         money = money + int(row[1])
+        if int(row[1]) > int(increase):
+            increase = row[1]
+            month_increase = row[0]
+        
+        if int(row[1]) < int(decrease):
+            decrease = row[1]
+            month_decrease = row[0]
 print(months)
 print(money)
+print(increase)
+print(month_increase)
+print(decrease)
+print(month_decrease)
 
