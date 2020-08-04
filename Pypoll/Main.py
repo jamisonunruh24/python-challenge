3486f0132d0b930b4f7f39eabbd2c08bc3531fc9
Jamison Unruh <jamisonunruh24@gmail.com>
2020-08-03 20:44:12 -0500

    added canidates list that adds canidate from csv

--- Pypoll/Main.py
+++ Pypoll/Main.py
@@ -5,6 +5,7 @@ import os
 # create path to file
 resources_path = os.path.join("Resources/" + "election_data.csv")
 total = 0
+canidate = []
 #read csv
 with open(resources_path) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -13,4 +14,7 @@ with open(resources_path) as csv_file:
 
     for row in csv_reader:
         total += 1
+        if row[2] not in canidate:
+            canidate.append(row[2])
 print(total)
+print(canidate)
